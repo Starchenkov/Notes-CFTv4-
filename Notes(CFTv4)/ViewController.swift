352e7f4f273260a0f39573c70d43892bb86c9d5c
Starchenkov <Starchenkov@live.ru>
2021-03-19 08:43:17 +0700

    Initial Commit

---
+++ Notes(CFTv4)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes(CFTv4)
+//
+//  Created by Sergey Starchenkov on 19.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
